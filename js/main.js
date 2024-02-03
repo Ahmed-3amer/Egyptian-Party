0e90848ed3901ac390802911a9e3d4f1734414ba
Ahmed-3amer <ahmedammer777@gmail.com>
2024-02-03 23:27:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,69 @@
+/* side bar event */
+$('.openIcon').click(function(){
+    $('.openNav').css('left', '0');
+    $('.openIcon').css('left', '-200px');
+})
+
+$('#closeIcon').click(function(){
+    $('.openNav').css('left', '-270px');
+    $('.openIcon').css('left', '-465px');
+})
+
+
+/* slide content event */
+$('.slideContent h3').click(function(){
+    $(this).next().slideToggle();
+    $('.slideContent p').not($(this).next()).slideUp();
+})
+
+
+/* counter */
+let countDownDate = new Date("Dec 31, 2024 20:00:00").getTime();
+
+let x = setInterval(function(){
+let now = new Date().getTime();
+let remainTime = countDownDate - now;
+
+var days = Math.floor(remainTime / (1000 * 60 * 60 * 24));
+var hours = Math.floor((remainTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((remainTime % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((remainTime % (1000 * 60)) / 1000);
+
+$('.daysCount').html(days + " " +"D")
+$('.hoursCount').html(hours + " " +"h")
+$('.minutesCount').html(minutes + " " +"m")
+$('.secondsCount').html(seconds + " " +"s")
+
+//this code below if i want to show EXPIRED and stop counter --
+
+// if(remainTime < 0){
+//     clearInterval(x);
+//     $('.expired').html('EXPIRED')
+// }
+
+}, 1000)
+
+
+/* contact */
+$('textarea').keyup(function(){
+    let myLength = $(this).val().length;
+    $('.num').text(
+        100 - myLength <= 0 ? "your available characters finished" : 100 - myLength 
+    )
+})
+
+/* scroll */
+$('.sectionLink').click(function(e){
+let aHref = e.target.getAttribute('href');
+let sectionTop = $(aHref).offset().top;
+console.log(sectionTop)
+$('body,html').animate({
+    scrollTop:sectionTop
+} , 2000)
+// $('body').animate({scrollTop:sectionOffset}, 10000)
+})
+
+// $('.sectionLink').click(function(e){
+//     let section = $(this).attr('href')
+//     console.log(section);
+// })
